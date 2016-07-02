47374aa843f93c03e7d040ee38380782c0f70896
Seeker1437 <williams.darnell56@yahoo.com>
2016-07-02 17:34:56 -0400

    Corrects mistake in MerchantAccountCapability

--- SquareConnect/V1/Enums/MerchantAccountCapability.cs
+++ SquareConnect/V1/Enums/MerchantAccountCapability.cs
@@ -5,7 +5,7 @@ namespace SquareConnect.V1.Enums
     /// <summary>
     /// Capabilities that a merchant's associated Square account might have.
     /// </summary>
-    public enum MercahtAccountCapability
+    public enum MerchantAccountCapability
     {
         /// <summary>
         /// The merchant can process credit cards with Square.
